89a36b02d63dd065f852e5a67cc4993461c99838
AmuruSaiPranathi <101181009+AmuruSaiPranathi@users.noreply.github.com>
2022-12-08 13:39:10 +0530

    Add files via upload

---
+++ registrationTaker.java
@@ -0,0 +1,152 @@
+
+public class RegistrationTakers {
+	public RegistrationTakers() {}
+	  
+     public RegistrationTakers(
+        Integer id, String firstName,
+        String lastName, String username, String password, String phone, String email)
+    {
+  
+        super();
+  
+        this.id = id;
+  
+        this.firstName = firstName;
+  
+        this.lastName = lastName;
+        
+        this.username = username;
+        
+        this.password = password;
+        
+        this.phone = phone;
+  
+        this.email = email;
+  
+           
+    }
+  
+       private Integer id;
+  
+       private String firstName;
+  
+       private String lastName;
+       
+       private String username;
+       
+       private String password;
+       
+       private String phone;
+  
+       private String email;
+       
+       
+  
+    
+    @Override
+   public String toString()
+    {
+  
+        return "Taker [id="
+            + id + ", firstName="
+            + firstName + ", lastName="
+            + lastName + ", email="
+            + email + "]";
+  
+           
+    }
+  
+    
+    public Integer getId()
+    {
+  
+         return id;
+    }
+  
+    public void setId(Integer id)
+    {
+  
+         this.id = id;
+    }
+  
+    public String getFirstName()
+    {
+  
+         return firstName;
+    }
+  
+    public void setFirstName(
+        String firstName)
+    {
+  
+         this.firstName = firstName;
+    }
+  
+    public String getLastName()
+    {
+  
+         return lastName;
+    }
+  
+  
+    public void setLastName(
+        String lastName)
+    {
+  
+         this.lastName = lastName;
+    }
+    
+    public String getusername()
+    {
+  
+         return username;
+    }
+  
+    
+    public void setusername(
+            String username)
+        {
+      
+             this.username = username;
+        }
+    public String getpassword()
+    {
+  
+         return password;
+    }
+    
+    public void setpassword(
+            String password)
+        {
+      
+             this.password = password;
+        }
+    
+    public String getphone()
+    {
+  
+         return phone;
+    }
+    
+    public void setphone(
+            String phone)
+        {
+      
+             this.phone = phone; 
+        }
+    
+      
+      
+  
+    public String getEmail()
+    {
+  
+         return email;
+    }
+  
+    public void setEmail(String email)
+    {
+  
+         this.email = email;
+    }
+}
